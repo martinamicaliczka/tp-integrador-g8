93da8b4b5842519075825221893e5c387fac7535
Briana_ML <brianamikk2@gamil.com>
2025-09-09 01:02:30 -0300

    listo el punto 8, de error

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer/Footer";
 import ResultadosBusqueda from "./screens/ResultadosBusqueda/ResultadosBusqueda";
 import Movie from "./screens/Movie/Movie";
 import Serie from "./screens/Serie/Serie";
+import Error404 from "./screens/Error/Error404";
 function App() {
   return (
     <React.Fragment>
@@ -14,6 +15,7 @@ function App() {
         <Route path='/results/:busuqeda' component={ResultadosBusqueda}/>
         <Route exact path="/movie/:id" component={Movie}/>
         <Route exact path="/serie/:id" component={Serie}/>
+        <Route component={Error404}/>
       </Switch>
       <Footer/>
     </React.Fragment>
---
+++ src/screens/Error/Error404.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import "./styles.css"
+export default class Error404 extends Component {
+    render() {
+        return (
+            <div className="nf-404">
+                <div className="nf-404__card">
+                    <div className="nf-404__code">
+                        404
+                    </div>
+                        <h2 className="nf-404__title">Contenido inexistente</h2>
+                        <p className="nf-404__text">La página que buscás no existe o fue movida. Probá volver al inicio o explorar nuestro catálogo.</p>
+                    <hr className="nf-404__divider" />
+                    <Link to="/" className="nf-404__btn nf-404__btn--primary">Volver al inicio</Link>
+                    <Link to="/movies/popular" className="nf-404__btn">Explorar películas</Link>
+                </div>
+            </div>
+        )
+    }
+}
